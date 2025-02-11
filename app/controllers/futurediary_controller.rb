d43485a06a2a681b6cc1a1b744d76056ae84b10c
mahiro1111 <148053248+mahiro1111@users.noreply.github.com>
2025-02-11 23:03:23 +0900

    controller作成

---
+++ app/controllers/futurediary_controller.rb
@@ -0,0 +1,2 @@
+class FuturediaryController < ApplicationController
+end
---
+++ test/controllers/futurediary_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FuturediaryControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
